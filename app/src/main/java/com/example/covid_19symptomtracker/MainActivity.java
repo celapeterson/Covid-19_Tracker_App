5443f66dceaa5d75aec1346298f9cec8afe67c79
Montelto <mcfarland3@wisc.edu>
2020-04-10 15:54:36 -0500

    Start

--- app/src/main/java/com/example/covid_19symptomtracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19symptomtracker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.covid_19symptomtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void symptomCheckButton(View view) {
+        Intent intent = new Intent(this, symptomcheck.class);
+        startActivity(intent);
+    }
+
+    public void symptomHistoryButton(View view) {
+        Intent intent = new Intent(this, symptomHistory.class);
+        startActivity(intent);
+    }
+
+    public void nearMeButton(View view) {
+        Intent intent = new Intent(this, nearMe.class);
+        startActivity(intent);
+    }
+
+    public void helpAvailableButton(View view) {
+        Intent intent = new Intent(this, helpAvailable.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/covid_19symptomtracker/symptomcheck.java
@@ -0,0 +1,13 @@
+package com.example.covid_19symptomtracker;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+class symptomcheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
