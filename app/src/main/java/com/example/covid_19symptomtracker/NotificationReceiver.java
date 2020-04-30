bdbb59ec173b6f17b6cea849a155faa0f73205e7
Montelto <mcfarland3@wisc.edu>
2020-04-30 12:35:47 -0500

    Tried starting Past results

--- app/src/main/java/com/example/covid_19symptomtracker/NotificationReceiver.java
+++ app/src/main/java/com/example/covid_19symptomtracker/NotificationReceiver.java
@@ -5,14 +5,11 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class NotificationReceiver extends BroadcastReceiver {
     public static String NOTIFICATION = "notification";
 
     public void onReceive(Context context, Intent intent) {
-        Log.i("1", "Schedule");
-
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
--- app/src/main/java/com/example/covid_19symptomtracker/PastResults.java
+++ app/src/main/java/com/example/covid_19symptomtracker/PastResults.java
@@ -1,13 +1,33 @@
 package com.example.covid_19symptomtracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.covid_19symptomtracker.database.Survey;
+
+import java.util.ArrayList;
+
 public class PastResults extends AppCompatActivity {
+    public static ArrayList<Survey> surveys = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.past_results_screen);
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("SymptomTrackerActivity.class",
+                Context.MODE_PRIVATE,null);
+    }
+
+    public void backButtonOnClick(View view) {
+        Intent intent = new Intent(this, StartingScreenActivity.class);
+        startActivity(intent);
     }
 }
